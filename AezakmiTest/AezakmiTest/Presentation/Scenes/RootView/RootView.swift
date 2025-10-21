17b74091b2e3d202cb75ad9aa776458e314c40bd
Niiaz Khasanov <nkhsnv@Niiazs-MacBook-Air.local>
2025-10-21 13:27:03 +0300

    step 1

---
+++ AezakmiTest/AezakmiTest/Presentation/Scenes/RootView/RootView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  AezakmiTest
+//
+//  Created by Niiaz Khasanov on 10/21/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        NavigationView {
+           WelcomeView()
+        }
+    }
+}
