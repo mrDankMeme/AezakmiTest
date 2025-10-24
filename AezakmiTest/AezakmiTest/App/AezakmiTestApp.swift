09cedda0f4abed102f2670771bcacd2ebb04d2ec
Niiaz Khasanov <nkhsnv@Niiazs-MacBook-Air.local>
2025-10-24 12:33:46 +0300

    core data update pt.2 austosave in AezakmiTestAoo

--- AezakmiTest/AezakmiTest/App/AezakmiTestApp.swift
+++ AezakmiTest/AezakmiTest/App/AezakmiTestApp.swift
@@ -5,20 +5,34 @@
 //  Created by Niiaz Khasanov on 10/21/25.
 //
 
-import SwiftUI
 
+import SwiftUI
+import UIKit
 
 @main
 struct AezakmiTestApp: App {
-    
+
     @MainActor private let root = CompositionRoot.shared
-    
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environment(\.managedObjectContext, root.coreData.viewContext)
                 .environment(\.documentRepository, root.documentRepository)
                 .environment(\.pdfService, root.pdf)
+                // На всякий случай сохраняем при завершении процесса
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)) { _ in
+                    root.coreData.saveContextIfNeeded()
+                }
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .background, .inactive:
+                root.coreData.saveContextIfNeeded()
+            default:
+                break
+            }
         }
     }
 }
